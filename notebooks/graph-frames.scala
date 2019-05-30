d90c733e362011f782bae0c3aa294cbe3c3f68c1
dmanibtech <dmanibtech@gmail.com>
2019-05-30 14:05:45 +0530

    Update: LICENSE README.md notebooks

---
+++ notebooks/graph-frames.scala
@@ -0,0 +1,12 @@
+// Databricks notebook source
+import org.apache.spark._
+import org.apache.spark.graphx._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.types.StructType
+import org.graphframes._
+import spark.implicits._
+
+// COMMAND ----------
+
